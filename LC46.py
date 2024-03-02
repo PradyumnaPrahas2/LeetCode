7addedbd4d6a74acc05cc61475773dfaf69768bb
PradyumnaPrahas2 <161730497+PradyumnaPrahas2@users.noreply.github.com>
2024-03-02 19:37:40 +0530

    Permutations

---
+++ LC46.py
@@ -0,0 +1,9 @@
+from itertools import permutations
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        l2=list(permutations(nums))
+        return l2
